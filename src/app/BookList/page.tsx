a06bbf81e88cd51e46640bb74c47b32d39611c9d
genie360s <alexgmkwizu@gmail.com>
2024-02-07 22:40:28 +0300

    added book images

--- src/app/BookList/page.tsx
+++ src/app/BookList/page.tsx
@@ -13,7 +13,7 @@ export default function BookList() {
         <div className="mt-[2vw]">
           <div className="lg:flex lg:flex-row gap-8  md:flex md:flex-col  ">
             <div className="lg:w-1/3 mt-8 rounded-lg h-96 shadow-xl hover:animate-button-bounce">
-              <div className="border-2 border-r-[8px] border-b-[8px] border-slate-700 relative flex place-content-center  p-1 bg-[url('/../images/image-a.jpg')] h-96 bg-cover rounded-lg">
+              <div className="border-2 border-r-[8px] border-b-[8px] border-slate-700 relative flex place-content-center  p-1 bg-[url('/../images/way-of-superior-man.jpg')] h-96 bg-cover rounded-lg">
                 <i className="text-4xl absolute -top-[12px] right-0 bi bi-bookmarks-fill text-slate-700"></i>
                 <button className="absolute bottom-[8px]  w-[12rem] border-2 border-orange-500 border-b-[5px] border-r-[5px]  rounded-lg text-center text-lg text-white font-bold p-2 mt-3 animate-button-bounce  ">
                   January
@@ -21,10 +21,66 @@ export default function BookList() {
               </div>
             </div>
             <div className="lg:w-1/3 mt-8   rounded-lg h-96 shadow-xl hover:animate-button-bounce">
-              <div className="border-2 border-r-[8px] border-b-[8px] border-slate-700 relative flex place-content-center  p-1 bg-[url('/../images/image-a.jpg')] h-96 bg-cover rounded-lg">
+              <div className="border-2 border-r-[8px] border-b-[8px] border-slate-700 relative flex place-content-center  p-1 bg-[url('/../images/no-more-mr-nice-guy.jpg')] h-96 bg-cover rounded-lg">
                 <i className="text-4xl absolute -top-[12px] right-0 bi bi-bookmarks-fill text-slate-700"></i>
                 <button className="absolute bottom-[8px]  w-[12rem] border-2 border-orange-500 border-b-[5px] border-r-[5px]  rounded-lg text-center text-lg text-white font-bold p-2 mt-3 animate-button-bounce  ">
-                  January
+                  February
+                </button>
+              </div>
+            </div>
+            <div className="lg:w-1/3 mt-8 rounded-lg h-96 shadow-xl hover:animate-button-bounce">
+              <div className="border-2 border-r-[8px] border-b-[8px] border-slate-700 relative flex place-content-center  p-1 bg-[url('/../images/atomic_habits.jpg')] h-96 bg-cover rounded-lg">
+                <i className="text-4xl absolute -top-[12px] right-0 bi bi-bookmarks-fill text-slate-700"></i>
+                <button className="absolute bottom-[8px]  w-[12rem] border-2 border-orange-500 border-b-[5px] border-r-[5px]  rounded-lg text-center text-lg text-white font-bold p-2 mt-3 animate-button-bounce  ">
+                  March
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-[5vw]">
+          <div className="lg:flex lg:flex-row gap-8 md:flex md:flex-col  ">
+            <div className="lg:w-1/3 mt-8 rounded-lg h-96 shadow-xl hover:animate-button-bounce">
+              <div className="border-2 border-r-[8px] border-b-[8px] border-slate-700 relative flex place-content-center  p-1 bg-[url('/../images/life-3-o.jpg')] h-96 bg-cover rounded-lg">
+                <i className="text-4xl absolute -top-[12px] right-0 bi bi-bookmarks-fill text-slate-700"></i>
+                <button className="absolute bottom-[8px]  w-[12rem] border-2 border-orange-500 border-b-[5px] border-r-[5px]  rounded-lg text-center text-lg text-white font-bold p-2 mt-3 animate-button-bounce  ">
+                  April
+                </button>
+              </div>
+            </div>
+            <div className="lg:w-1/3 mt-8   rounded-lg h-96 shadow-xl hover:animate-button-bounce">
+              <div className="border-2 border-r-[8px] border-b-[8px] border-slate-700 relative flex place-content-center  p-1 bg-[url('/../images/48rulesofpower.jpg')] h-96 bg-cover rounded-lg">
+                <i className="text-4xl absolute -top-[12px] right-0 bi bi-bookmarks-fill text-slate-700"></i>
+                <button className="absolute bottom-[8px]  w-[12rem] border-2 border-orange-500 border-b-[5px] border-r-[5px]  rounded-lg text-center text-lg text-white font-bold p-2 mt-3 animate-button-bounce  ">
+                  May
+                </button>
+              </div>
+            </div>
+            <div className="lg:w-1/3 mt-8 rounded-lg h-96 shadow-xl hover:animate-button-bounce">
+              <div className="border-2 border-r-[8px] border-b-[8px] border-slate-700 relative flex place-content-center  p-1 bg-[url('/../images/doac.jpg')] h-96 bg-cover rounded-lg">
+                <i className="text-4xl absolute -top-[12px] right-0 bi bi-bookmarks-fill text-slate-700"></i>
+                <button className="absolute bottom-[8px]  w-[12rem] border-2 border-orange-500 border-b-[5px] border-r-[5px]  rounded-lg text-center text-lg text-white font-bold p-2 mt-3 animate-button-bounce  ">
+                  June
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-[5vw]">
+          <div className="lg:flex lg:flex-row gap-8 md:flex md:flex-col  ">
+            <div className="lg:w-1/3 mt-8 rounded-lg h-96 shadow-xl hover:animate-button-bounce">
+              <div className="border-2 border-r-[8px] border-b-[8px] border-slate-700 relative flex place-content-center  p-1 bg-[url('/../images/eatthatfrog.jpg')] h-96 bg-cover rounded-lg">
+                <i className="text-4xl absolute -top-[12px] right-0 bi bi-bookmarks-fill text-slate-700"></i>
+                <button className="absolute bottom-[8px]  w-[12rem] border-2 border-orange-500 border-b-[5px] border-r-[5px]  rounded-lg text-center text-lg text-white font-bold p-2 mt-3 animate-button-bounce  ">
+                  July
+                </button>
+              </div>
+            </div>
+            <div className="lg:w-1/3 mt-8   rounded-lg h-96 shadow-xl hover:animate-button-bounce">
+              <div className="border-2 border-r-[8px] border-b-[8px] border-slate-700 relative flex place-content-center  p-1 bg-[url('/../images/robin_sharma.jpg')] h-96 bg-cover rounded-lg">
+                <i className="text-4xl absolute -top-[12px] right-0 bi bi-bookmarks-fill text-slate-700"></i>
+                <button className="absolute bottom-[8px]  w-[12rem] border-2 border-orange-500 border-b-[5px] border-r-[5px]  rounded-lg text-center text-lg text-white font-bold p-2 mt-3 animate-button-bounce  ">
+                  August
                 </button>
               </div>
             </div>
@@ -32,7 +88,7 @@ export default function BookList() {
               <div className="border-2 border-r-[8px] border-b-[8px] border-slate-700 relative flex place-content-center  p-1 bg-[url('/../images/image-a.jpg')] h-96 bg-cover rounded-lg">
                 <i className="text-4xl absolute -top-[12px] right-0 bi bi-bookmarks-fill text-slate-700"></i>
                 <button className="absolute bottom-[8px]  w-[12rem] border-2 border-orange-500 border-b-[5px] border-r-[5px]  rounded-lg text-center text-lg text-white font-bold p-2 mt-3 animate-button-bounce  ">
-                  January
+                  September
                 </button>
               </div>
             </div>
@@ -44,7 +100,7 @@ export default function BookList() {
               <div className="border-2 border-r-[8px] border-b-[8px] border-slate-700 relative flex place-content-center  p-1 bg-[url('/../images/image-a.jpg')] h-96 bg-cover rounded-lg">
                 <i className="text-4xl absolute -top-[12px] right-0 bi bi-bookmarks-fill text-slate-700"></i>
                 <button className="absolute bottom-[8px]  w-[12rem] border-2 border-orange-500 border-b-[5px] border-r-[5px]  rounded-lg text-center text-lg text-white font-bold p-2 mt-3 animate-button-bounce  ">
-                  January
+                  October
                 </button>
               </div>
             </div>
@@ -52,7 +108,7 @@ export default function BookList() {
               <div className="border-2 border-r-[8px] border-b-[8px] border-slate-700 relative flex place-content-center  p-1 bg-[url('/../images/image-a.jpg')] h-96 bg-cover rounded-lg">
                 <i className="text-4xl absolute -top-[12px] right-0 bi bi-bookmarks-fill text-slate-700"></i>
                 <button className="absolute bottom-[8px]  w-[12rem] border-2 border-orange-500 border-b-[5px] border-r-[5px]  rounded-lg text-center text-lg text-white font-bold p-2 mt-3 animate-button-bounce  ">
-                  January
+                  November
                 </button>
               </div>
             </div>
@@ -60,63 +116,7 @@ export default function BookList() {
               <div className="border-2 border-r-[8px] border-b-[8px] border-slate-700 relative flex place-content-center  p-1 bg-[url('/../images/image-a.jpg')] h-96 bg-cover rounded-lg">
                 <i className="text-4xl absolute -top-[12px] right-0 bi bi-bookmarks-fill text-slate-700"></i>
                 <button className="absolute bottom-[8px]  w-[12rem] border-2 border-orange-500 border-b-[5px] border-r-[5px]  rounded-lg text-center text-lg text-white font-bold p-2 mt-3 animate-button-bounce  ">
-                  January
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="mt-[5vw]">
-          <div className="lg:flex lg:flex-row gap-8 md:flex md:flex-col  ">
-            <div className="lg:w-1/3 mt-8 rounded-lg h-96 shadow-xl hover:animate-button-bounce">
-              <div className="border-2 border-r-[8px] border-b-[8px] border-slate-700 relative flex place-content-center  p-1 bg-[url('/../images/image-a.jpg')] h-96 bg-cover rounded-lg">
-                <i className="text-4xl absolute -top-[12px] right-0 bi bi-bookmarks-fill text-slate-700"></i>
-                <button className="absolute bottom-[8px]  w-[12rem] border-2 border-orange-500 border-b-[5px] border-r-[5px]  rounded-lg text-center text-lg text-white font-bold p-2 mt-3 animate-button-bounce  ">
-                  January
-                </button>
-              </div>
-            </div>
-            <div className="lg:w-1/3 mt-8   rounded-lg h-96 shadow-xl hover:animate-button-bounce">
-              <div className="border-2 border-r-[8px] border-b-[8px] border-slate-700 relative flex place-content-center  p-1 bg-[url('/../images/image-a.jpg')] h-96 bg-cover rounded-lg">
-                <i className="text-4xl absolute -top-[12px] right-0 bi bi-bookmarks-fill text-slate-700"></i>
-                <button className="absolute bottom-[8px]  w-[12rem] border-2 border-orange-500 border-b-[5px] border-r-[5px]  rounded-lg text-center text-lg text-white font-bold p-2 mt-3 animate-button-bounce  ">
-                  January
-                </button>
-              </div>
-            </div>
-            <div className="lg:w-1/3 mt-8 rounded-lg h-96 shadow-xl hover:animate-button-bounce">
-              <div className="border-2 border-r-[8px] border-b-[8px] border-slate-700 relative flex place-content-center  p-1 bg-[url('/../images/image-a.jpg')] h-96 bg-cover rounded-lg">
-                <i className="text-4xl absolute -top-[12px] right-0 bi bi-bookmarks-fill text-slate-700"></i>
-                <button className="absolute bottom-[8px]  w-[12rem] border-2 border-orange-500 border-b-[5px] border-r-[5px]  rounded-lg text-center text-lg text-white font-bold p-2 mt-3 animate-button-bounce  ">
-                  January
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="mt-[5vw]">
-          <div className="lg:flex lg:flex-row gap-8 md:flex md:flex-col  ">
-            <div className="lg:w-1/3 mt-8 rounded-lg h-96 shadow-xl hover:animate-button-bounce">
-              <div className="border-2 border-r-[8px] border-b-[8px] border-slate-700 relative flex place-content-center  p-1 bg-[url('/../images/image-a.jpg')] h-96 bg-cover rounded-lg">
-                <i className="text-4xl absolute -top-[12px] right-0 bi bi-bookmarks-fill text-slate-700"></i>
-                <button className="absolute bottom-[8px]  w-[12rem] border-2 border-orange-500 border-b-[5px] border-r-[5px]  rounded-lg text-center text-lg text-white font-bold p-2 mt-3 animate-button-bounce  ">
-                  January
-                </button>
-              </div>
-            </div>
-            <div className="lg:w-1/3 mt-8   rounded-lg h-96 shadow-xl hover:animate-button-bounce">
-              <div className="border-2 border-r-[8px] border-b-[8px] border-slate-700 relative flex place-content-center  p-1 bg-[url('/../images/image-a.jpg')] h-96 bg-cover rounded-lg">
-                <i className="text-4xl absolute -top-[12px] right-0 bi bi-bookmarks-fill text-slate-700"></i>
-                <button className="absolute bottom-[8px]  w-[12rem] border-2 border-orange-500 border-b-[5px] border-r-[5px]  rounded-lg text-center text-lg text-white font-bold p-2 mt-3 animate-button-bounce  ">
-                  January
-                </button>
-              </div>
-            </div>
-            <div className="lg:w-1/3 mt-8 rounded-lg h-96 shadow-xl hover:animate-button-bounce">
-              <div className="border-2 border-r-[8px] border-b-[8px] border-slate-700 relative flex place-content-center  p-1 bg-[url('/../images/image-a.jpg')] h-96 bg-cover rounded-lg">
-                <i className="text-4xl absolute -top-[12px] right-0 bi bi-bookmarks-fill text-slate-700"></i>
-                <button className="absolute bottom-[8px]  w-[12rem] border-2 border-orange-500 border-b-[5px] border-r-[5px]  rounded-lg text-center text-lg text-white font-bold p-2 mt-3 animate-button-bounce  ">
-                  January
+                  December
                 </button>
               </div>
             </div>
